7e8579abaa8c92a25a1d29cb9b9e45d165273479
sergiopalooza <sflores@precognitive.io>
2020-01-27 16:16:43 -0600

    moved over old code

---
+++ index.js
@@ -0,0 +1,30 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+async function run() {
+  const [owner, repo] = process.env.GITHUB_REPOSITORY.split('/');
+  const { number } = github.context.payload;
+
+  const token = core.getInput('token');
+  const octokit = new github.GitHub(token);
+
+  try {
+    await octokit.pulls.createReviewRequest({
+      owner,
+      repo,
+      pull_number: number,
+      team_reviewers: ['software-engineers']
+    });
+
+    await octokit.issues.addAssignees({
+      owner,
+      repo,
+      issue_number: number,
+      assignees: ['zrosenbauer', 'polson136']
+    });
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
